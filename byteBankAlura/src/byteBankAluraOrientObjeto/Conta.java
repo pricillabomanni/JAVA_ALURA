106f97f0923f6c6cf82f6fb8c5819e2db3bd9e35
Pricilla Souza <pricilla_souzacs@hotmail.com>
2020-05-19 22:18:14 -0300

    Subindo byteBankAlura

---
+++ byteBankAlura/src/byteBankAluraOrientObjeto/Conta.java
@@ -0,0 +1,9 @@
+package byteBankAluraOrientObjeto;
+
+public class Conta {
+	public double salario;
+	public int agencia;
+	public int numero;
+	public String titular;
+	public int saldo;
+}
---
+++ byteBankAlura/src/byteBankAluraOrientObjeto/InstanciandoSegundaConta.java
@@ -0,0 +1,12 @@
+package byteBankAluraOrientObjeto;
+
+public class InstanciandoSegundaConta {
+	public static void main(String[] args) {
+
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 500;
+		System.out.println(segundaConta.saldo);
+
+	}
+
+}
